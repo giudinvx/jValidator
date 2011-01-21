522ffd55667dae0534f20b6826ab81d9c54b90bd
Giuseppe D'Inverno <giudinvx@gmail.com>
2011-01-21 18:23:18 +0100

    other check

--- jvalidator.js
+++ jvalidator.js
@@ -29,6 +29,17 @@ String.prototype.ipCheck = function () {
 	}
 };
 
+String.prototype.macCheck = function () {
+	var macCheck = /([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}/i;
+	var macTrue = this.match(macCheck);
+	
+	if (macTrue) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
 String.prototype.emailCheck = function () {
 	var emailRegex = /^(([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+([;.](([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5}){1,25})+)*$/i;
 	var emailTrue = this.match(emailRegex);
@@ -51,3 +62,50 @@ String.prototype.urlCheck = function () {
 		return false;
 	}
 };
+
+String.prototype.ibanCheck = function () {
+	var ibanRegex = /[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}/i;
+	var ibanTrue = this.match(ibanRegex);
+	
+	if (ibanTrue) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+String.prototype.bicCheck = function () {
+	var bicRegex = /([a-zA-Z]{4}[a-zA-Z]{2}[a-zA-Z0-9]{2}([a-zA-Z0-9]{3})?)/i;
+	var bicTrue = this.match(bicRegex);
+	
+	if (bicTrue) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+String.prototype.zipcodeCheck = function () {
+	var zipcodeRegex = /^[0-9]{5}(-[0-9]{4})?$/i;
+	var zipcodeTrue = this.match(zipcodeRegex);
+	
+	if (zipcodeTrue) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+String.prototype.timeCheck = function () { 
+	var timeRegex = /^([1-9]|1[0-2]):[0-5]\d(:[0-5]\d(\.\d{1,3})?)?$/i;
+	var timeTrue = this.match(timeRegex);
+	
+	if (timeTrue) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+///(#([0-9A-Fa-f]{3}){1,2}\b)/
+
